0f39fa610bf4ef240837d7451f90ac445bce0c6b
toplancer1204 <pro.developer1204@gmail.com>
2020-08-27 19:47:58 +0200

    User Controller

---
+++ klout-server/controllers/user.controller.js
@@ -0,0 +1,49 @@
+const User = require("../models/user.model");
+const mongoose = require("mongoose");
+
+class UserController {
+  static async get(_id) {
+    return await User.findById(_id);
+  }
+
+  static async set(user) {
+    try {
+      const editUser = await User.findByIdAndUpdate(user._id, user, {
+        new: true,
+      });
+      return editUser;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async addUser(user) {
+    const { username, name, email, password } = user;
+    return await UserController.add(username, name, email, password);
+  }
+
+  static async add(username, name, email, password) {
+    try {
+      const newUser = await User.create({ username, name, email, password });
+      return newUser;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  static async delete(id) {
+    const delUser = await User.findByIdAndRemove(id);
+    return delUser;
+  }
+  static async list() {
+    return await User.find();
+  }
+  static async findByEmail(email) {
+    return await User.findOne({ email });
+  }
+  static async findByUsername(username) {
+    return await User.findOne({ username });
+  }
+}
+
+module.exports = UserController;
---
+++ klout-server/routes/auth.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+;
+
+router.post('/login', (req, res, next) => {});
+
+router.post('/signup', async (req, res, next) => {});
+
+router.post('/logout', (req, res, next) => {});
+
