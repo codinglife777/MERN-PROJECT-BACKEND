29aec37eec3354e4207a5f559222115e6793ee34
toplancer1204 <pro.developer1204@gmail.com>
2020-08-27 14:13:12 +0200

    Rename App

---
+++ klout-server/models/network.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const NetworkSchema = new mongoose.Schema(
+  {
+    facebook: {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+    twitter: {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+    instagram: {
+        username: { type: String, required: true },
+        password: { type: String, required: true },
+    },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },,
+    
+  },
+  { timestamps: true }
+);
+
+const Network = mongoose.model("Network", NetworkSchema);
+
+module.exports = Network;
