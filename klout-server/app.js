bf7192f94e1a813309479d7c3b572bdc407f2f61
toplancer1204 <pro.developer1204@gmail.com>
2020-09-11 19:42:14 +0200

    Update Profile

--- klout-server/app.js
+++ klout-server/app.js
@@ -8,8 +8,50 @@ const logger = require("morgan");
 const path = require("path");
 const cors = require("cors");
 const passport = require("passport");
+/*const session = require('express-session')
+const socketio = require('socket.io')
+const authRouter = require('./configs/auth.router')
+const passportInit = require('./configs/passport.init')*/
+/*const { SESSION_SECRET, CLIENT_ORIGIN } = require('./config')*/
 
 const app = express();
+
+/*const certOptions = {
+  key: fs.readFileSync(path.resolve('certs/server.key')),
+  cert: fs.readFileSync(path.resolve('certs/server.crt'))
+}*/
+
+/*const server = https.createServer(certOptions, app)*/
+
+// Setup for passport and to accept JSON objects
+/*app.use(express.json())
+app.use(passport.initialize())*/
+/*passportInit()*/
+
+// Accept requests from the client
+/*app.use(cors({
+  origin: CLIENT_ORIGIN
+})) */
+
+// saveUninitialized: true allows us to attach the socket id to the session
+// before we have athenticated the user
+/*app.use(session({ 
+  secret: process.env.SESSION_SECRET, 
+  resave: true, 
+  saveUninitialized: true 
+}))*/
+
+// Connecting sockets to the server and adding them to the request 
+// so that we can access them later in the controller
+/*const io = socketio(server)
+app.set('io', io)*/
+
+// Direct all requests to the auth router
+/*app.use('/', authRouter)
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log('listening...')
+})*/
 
 require("dayjs/locale/es");
 require("./configs/db.config");
---
+++ klout-server/configs/passport.init.js
@@ -0,0 +1,28 @@
+
+
+const passport = require('passport')
+const { Strategy: TwitterStrategy } = require('passport-twitter')
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth')
+const { Strategy: FacebookStrategy } = require('passport-facebook')
+const { Strategy: GithubStrategy} = require('passport-github')
+/*const { 
+  TWITTER_CONFIG, GOOGLE_CONFIG, FACEBOOK_CONFIG, GITHUB_CONFIG
+} = require('../config')*/
+
+module.exports = () => {  
+
+  // Allowing passport to serialize and deserialize users into sessions
+  passport.serializeUser((user, cb) => cb(null, user))
+  passport.deserializeUser((obj, cb) => cb(null, obj))
+  
+  // The function that is called when an OAuth provider sends back user 
+  // information.  Normally, you would save the user to the database here
+  // in a callback that was customized for each provider.
+  const callback = (accessToken, refreshToken, profile, cb) => cb(null, profile)
+
+  // Adding each OAuth provider's strategy to passport
+  passport.use(new TwitterStrategy(TWITTER_CONFIG, callback))
+  passport.use(new GoogleStrategy(GOOGLE_CONFIG, callback))
+  passport.use(new FacebookStrategy(FACEBOOK_CONFIG, callback))
+  passport.use(new GithubStrategy(GITHUB_CONFIG, callback))
+}
