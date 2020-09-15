3e35f830b2aaaf83e36062cd4044e9cfa0608a1d
toplancer1204 <pro.developer1204@gmail.com>
2020-09-15 15:28:13 +0200

    initial commit to heroku

---
+++ klout-server/public/precache-manifest.38661dcba89ca7d8cf93c91268ea635d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a0cba6fe308d339b9dab50a710c4b26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19641fa77033cf0ab13b",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "17b5cc783ddf9518b771",
+    "url": "/static/css/main.784cd118.chunk.css"
+  },
+  {
+    "revision": "19641fa77033cf0ab13b",
+    "url": "/static/js/2.7dc9e4e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7dc9e4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b5cc783ddf9518b771",
+    "url": "/static/js/main.a1037877.chunk.js"
+  },
+  {
+    "revision": "87414586ae515e875d1c",
+    "url": "/static/js/runtime-main.3e0e9719.js"
+  }
+]);
